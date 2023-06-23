55e88fa5f006656ee2e0481699a0277a2ef11ece
ppayal8866 <ppayal8866@gmail.com>
2023-06-22 22:33:49 -0700

    Modified UI

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.loginapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,20 +12,18 @@ import soup.neumorphism.NeumorphButton;
 
 public class MainActivity extends AppCompatActivity {
     String user, pass;
-    EditText email, password;
-    NeumorphButton login, register;
-    CheckBox remember;
+    EditText user_input, enter_otp;
+    NeumorphButton get_otp, verify_otp;
     SharedPreferences preferences, rem, logged, userin, check;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-        register = findViewById(R.id.register);
-        remember = findViewById(R.id.remember);
+        user_input = findViewById(R.id.user_input);
+        enter_otp = findViewById(R.id.enter_otp);
+        get_otp = findViewById(R.id.get_otp);
+        verify_otp = findViewById(R.id.verify_otp);
 
         preferences = getSharedPreferences("Userinfo", MODE_PRIVATE);
         rem = getSharedPreferences("saveLogin", MODE_PRIVATE);
@@ -45,44 +42,32 @@ public class MainActivity extends AppCompatActivity {
         int remValue = rem.getInt("saveLogin", 0);
 
         if(remValue ==1){
-            email.setText(user);
-            password.setText(pass);
-            remember.setChecked(true);
+            user_input.setText(user);
+            enter_otp.setText(pass);
 
         }
 
         SharedPreferences.Editor editor = rem.edit();
-        remember.setOnCheckedChangeListener((buttonView, isChecked) -> {
-
-            if(remember.isChecked()){
-                editor.putInt("saveLogin", 1);
-                editor.apply();
-            }else if(!remember.isChecked()){
-                editor.putInt("saveLogin", 0);
-                editor.apply();
-            }
-        });
 
 
-        login.setOnClickListener(v -> {
+        get_otp.setOnClickListener(v -> {
 
-            if (email.getText().toString().equals(user) && password.getText().toString().equals(pass) && !(user.equals("")) && !(pass.equals(""))) {
-                if(!remember.isChecked()){
-                    email.setText("");
-                    password.setText("");
-                }
+            if (user_input.getText().toString().equals(user) && enter_otp.getText().toString().equals(pass) && !(user.equals("")) && !(pass.equals(""))) {
+                // todo: maybe remove this?
+                user_input.setText("");
+                enter_otp.setText("");
 
                 Intent intent = new Intent(MainActivity.this, Home.class);
                 startActivity(intent);
                 logged.edit().putBoolean("Inlogged", true).apply();
-            } else if(email.getText().toString().equals("") || password.getText().toString().equals("")) {
+            } else if(user_input.getText().toString().equals("") || enter_otp.getText().toString().equals("")) {
                 Toast.makeText(MainActivity.this, "Enter credentials", Toast.LENGTH_SHORT).show();
             } else{
                 Toast.makeText(MainActivity.this, "Invalid User", Toast.LENGTH_SHORT).show();
             }
         });
 
-        register.setOnClickListener(v -> {
+        verify_otp.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Register.class);
             startActivity(intent);
         });
